20a3dfe147b3b917784a42b1fc01fb85d78fdabd
kolandaina <3361716526@qq.com>
2024-09-02 16:57:42 +0800

    main

--- C/code/test/1.cpp
+++ C/code/test/1.cpp
@@ -7,30 +7,38 @@ const int mod = 1e9 + 7;
 const int INF = INT64_MAX;
 #define fs(x) std::fixed << std::setprecision(x)
 inline void solve() {
-    int n, x;
-    std::cin >> n >> x;
+    int n;
+    std::cin >> n;
     std::vector<std::vector<int>> g(n + 1);
-    std::vector<int> in(n + 1);
-    for (int i = 1; i < n; ++i) {
+    for (int i = 1; i < n; i++) {
         int u, v;
         std::cin >> u >> v;
         g[u].push_back(v);
         g[v].push_back(u);
-        in[u]++, in[v]++;
     }
-    if (n == 1)std::cout << "Ayush" << endl;
-    else {
-        if (in[x] == 1)std::cout << "Ayush" << endl;
-        else if (n % 2)std::cout << "Ashish" << endl;
-        else std::cout << "Ayush" << endl;
+    int ans = 0;
+    std::vector<int> size(n + 1);
+    std::function<void(int, int)> dfs = [&](int u, int fa) {
+        size[u] = 1;
+        for (auto v : g[u]) {
+            if (v == fa) continue;
+            dfs(v, u);
+            size[u] += size[v];
+        }
+    };
+    dfs(1, 0);
+    for (int i = 1; i <= n; i++) {
+        if (size[i] == 1)ans++;
     }
+    if (n == 2)std::cout << 2 << endl;
+    else std::cout << ans << endl;
 }
 signed main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
     int t = 1;
-    std:: cin >> t;
+    // std:: cin >> t;
     while (t--) solve();
     return 0;
 }
--- C/code/test/2.cpp
+++ C/code/test/2.cpp
@@ -1,75 +1,68 @@
 #include<bits/stdc++.h>
-#include<functional>
-#define int long long
-#define endl "\n"
-const int N = 2e5 + 10;
-const int mod = 1e9 + 7;
-const int INF = INT64_MAX;
-#define fs(x) std::fixed << std::setprecision(x)
-struct BIT {
-    int n;
-    std::vector<int> w;
-    BIT() {}
-    BIT(int n) {
-        this->n = n;
-        w.resize(n + 1);
+using namespace std;
+const int N = 5e5 + 7;
+char s1[N], s2[N];
+struct suffixautomaton {
+    int sz = 1;
+    int last = 1;
+    int trans[2 * N][27], link[2 * N], siz[2 * N][2], lst[2 * N];
+    int head[2 * N], nxt[2 * N], ver[2 * N], tot = 1;
+    int ans = 0;
+    inline void add(int x, int y) {
+        ver[++tot] = y, nxt[tot] = head[x], head[x] = tot;
     }
-    void add(int x, int k) {
-        for (; x <= n; x += x & -x) {
-            w[x] += k;
+    void insert(int val, int o) {
+        int now = ++sz;
+        lst[now] = lst[last] + 1;
+        int p = last;
+        siz[now][o]++;
+        while (!trans[p][val]) {
+            trans[p][val] = now;
+            p = link[p];
         }
-    }
-    void add(int x, int y, int k) {
-        add(x, k), add(y, -k);
-    }
-    int ask(int x) {
-        int ans = 0;
-        for (; x; x -= x & -x) {
-            ans += w[x];
+        if (p == 0) {
+            link[last = now] = 1;
+            return;
         }
-        return ans;
-    }
-    int ask(int x, int y) {
-        return ask(y) - ask(x - 1);
-    }
-};
-inline void solve() {
-    std::string s;
-    int len, q;
-    std::cin >> s;
-    len = s.length();
-    std::vector<BIT> tree(26, BIT(len));
-    for (int i = 0; i < len; i++) {
-        tree[s[i] - 'a'].add(i + 1, 1);
-    }
-    std::cin >> q;
-    while (q--) {
-        int opt, x, y;
-        char z;
-        std::cin >> opt;
-        if (opt == 1) {
-            std::cin >> x >> z;
-            tree[s[x - 1] - 'a'].add(x, -1);
-            tree[z - 'a'].add(x, 1);
-            s[x - 1] = z;
-        } else if (opt == 2) {
-            std::cin >> x >> y;
-            int ans = 0;
-            for (int i = 0; i < 26; i++) {
-                if (tree[i].ask(x, y) > 0) {
-                    ans++;
-                }
+        int q = trans[p][val];
+        if (lst[q] == lst[p] + 1) {
+            link[now] = q;
+        } else {
+            int clone = ++sz;
+            lst[clone] = lst[p] + 1;
+            link[clone] = link[q];
+            memcpy(trans[clone], trans[q], sizeof(trans[q]));
+            while (trans[p][val] == q) {
+                trans[p][val] = clone;
+                p = link[p];
             }
-            std::cout << ans << endl;
+            link[now] = link[q] = clone;
         }
+        last = now;
     }
-}
-signed main() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    int t = 1;
-    //std:: cin >> t;
-    while (t--) solve();
-    return 0;
+    inline void build() {
+        for (int i = 2; i <= sz; i++)
+            add(link[i], i);
+    }
+    void dfs(int x) {
+        for (int i = head[x]; i; i = nxt[i]) {
+            int y = ver[i];
+            dfs(y);
+            siz[x][0] += siz[y][0], siz[x][1] += siz[y][1];
+        }
+        if (siz[x][0] && siz[x][1]) ans = max(ans, lst[x]);
+    }
+} sam;
+int main() {
+    scanf("%s", s1);
+    scanf("%s", s2);
+    int len1 = strlen(s1), len2 = strlen(s2);
+    for (int i = 0; i < len1; i++)
+        sam.insert(s1[i] - 'a', 0);
+    sam.insert(26, 0);
+    for (int i = 0; i < len2; i++)
+        sam.insert(s2[i] - 'a', 1);
+    sam.build();
+    sam.dfs(1);
+    printf("%d\n", sam.ans);
 }
