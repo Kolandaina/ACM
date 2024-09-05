632633c8b34ce1c42ffc63d2a4a46f27d9da0570
kolandaina <3361716526@qq.com>
2024-09-05 09:43:16 +0800

    main

--- C/code/test/2.cpp
+++ C/code/test/2.cpp
@@ -1,68 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-const int N = 5e5 + 7;
-char s1[N], s2[N];
-struct suffixautomaton {
-    int sz = 1;
-    int last = 1;
-    int trans[2 * N][27], link[2 * N], siz[2 * N][2], lst[2 * N];
-    int head[2 * N], nxt[2 * N], ver[2 * N], tot = 1;
-    int ans = 0;
-    inline void add(int x, int y) {
-        ver[++tot] = y, nxt[tot] = head[x], head[x] = tot;
-    }
-    void insert(int val, int o) {
-        int now = ++sz;
-        lst[now] = lst[last] + 1;
-        int p = last;
-        siz[now][o]++;
-        while (!trans[p][val]) {
-            trans[p][val] = now;
-            p = link[p];
-        }
-        if (p == 0) {
-            link[last = now] = 1;
-            return;
-        }
-        int q = trans[p][val];
-        if (lst[q] == lst[p] + 1) {
-            link[now] = q;
-        } else {
-            int clone = ++sz;
-            lst[clone] = lst[p] + 1;
-            link[clone] = link[q];
-            memcpy(trans[clone], trans[q], sizeof(trans[q]));
-            while (trans[p][val] == q) {
-                trans[p][val] = clone;
-                p = link[p];
-            }
-            link[now] = link[q] = clone;
-        }
-        last = now;
-    }
-    inline void build() {
-        for (int i = 2; i <= sz; i++)
-            add(link[i], i);
-    }
-    void dfs(int x) {
-        for (int i = head[x]; i; i = nxt[i]) {
-            int y = ver[i];
-            dfs(y);
-            siz[x][0] += siz[y][0], siz[x][1] += siz[y][1];
-        }
-        if (siz[x][0] && siz[x][1]) ans = max(ans, lst[x]);
-    }
-} sam;
+using LL = long long;
+const int mod = 20220911;
 int main() {
-    scanf("%s", s1);
-    scanf("%s", s2);
-    int len1 = strlen(s1), len2 = strlen(s2);
-    for (int i = 0; i < len1; i++)
-        sam.insert(s1[i] - 'a', 0);
-    sam.insert(26, 0);
-    for (int i = 0; i < len2; i++)
-        sam.insert(s2[i] - 'a', 1);
-    sam.build();
-    sam.dfs(1);
-    printf("%d\n", sam.ans);
+    ios::sync_with_stdio(false); cin.tie(0);
+    string s;
+    getline(cin, s);
+    int n = s.size();
+    vector <string> a;
+    for (int i = 0; i < n; i ++) {
+        if (s[i] == ' ') continue;
+        string t = "";
+        for (; i < n && s[i] != ' '; i ++)
+            t = t + s[i];
+        a.push_back(t);
+    }
+    int m = a.size();
+    vector <LL> b;
+    b.push_back(0);
+    for (int i = 0; i < m; i ++) {
+        if (a[i] == "library") {
+            b.back() = (b.back() + 1) % mod;
+        } else if (a[i] == "repeat") {
+            b.push_back(0);
+        } else if ('0' <= a[i][0] && a[i][0] <= '9') {
+            b.back() = b.back() * stoi(a[i]) % mod;
+        } else if (a[i] == "times") {
+            LL t = b.back();
+            b.pop_back();
+            b.back() = (b.back() + t) % mod;
+        }
+    }
+    cout << b.back() << "\n";
+    return 0;
 }
