b254c0a95de08988f66a2de030a133c7a38764dc
kolandaina <3361716526@qq.com>
2024-09-06 18:57:27 +0800

    main

--- C/code/test/1.cpp
+++ C/code/test/1.cpp
@@ -1,22 +1,54 @@
+#define  _CRT_SECURE_NO_WARNINGS
 #include<bits/stdc++.h>
-#include<functional>
+using namespace std;
 #define int long long
-#define endl "\n"
-const int N = 2e5 + 10;
-const int mod = 1e9 + 7;
-const int INF = INT64_MAX;
-#define fs(x) std::fixed << std::setprecision(x)
-inline void solve() {
-    int n, m;
-    std::cin >> n >> m;
-    std::cout << "1/" << n << endl;
+const int p = 2e5 + 10, mod = 1e9 + 7;
+
+int n;
+map<pair<int, int>, int>mp1;
+struct node {
+    int x, y;
+} a[p];
+bool cmp(node a, node b) {
+    if (a.x == b.x)return a.y < b.y;
+    return a.x < b.x;
 }
+
 signed main() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    int t = 1;
-    // std:: cin >> t;
-    while (t--) solve();
-    return 0;
+    cin >> n;
+    string s;
+    cin >> s;
+    int cnt = 0;
+    for (int i = 0; i < (1 << n); i++) {
+        int x = 0, y = 0;
+        map<pair<int, int>, int>mp;
+        int f = 0;
+        mp[ {0, 0}] = 1;
+        for (int j = 0; j < n; j++) {
+            int dx = 0, dy = 0;
+            if (s[j] == 'L')dx = -1;
+            else if (s[j] == 'R')dx = 1;
+            else if (s[j] == 'U')dy = 1;
+            else if (s[j] == 'D')dy = -1;
+            if (i & (1 << j)) {
+                x += dx, y += dy;
+                mp[ {x, y}] = 1;
+            } else {
+                if (mp[ {x + dx, y + dy}]) {
+                    f = 1;
+                    break;
+                }
+            }
+        }
+        if (f == 0 && mp1[ {x, y}] == 0) {
+            a[++cnt].x = x;
+            a[cnt].y = y;
+            mp1[ {x, y}] = 1;
+        }
+    }
+    printf("%lld\n", cnt);
+    sort(a + 1, a + 1 + cnt, cmp);
+    for (int i = 1; i <= cnt; i++) {
+        printf("%lld %lld\n", a[i].x, a[i].y);
+    }
 }
--- C/code/test/2.cpp
+++ C/code/test/2.cpp
@@ -1,33 +1,99 @@
-#include <iostream>
-#include<vector>
-#include<deque>
-using namespace std;
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int m = 12;
-        int a, b;
-        int now[4] = {0, 2, 3, 4};
-        deque<int> v[20];
-        for (int i = 1; i <= 3; i++)
-            v[now[i]].push_back(i);
-        while (m--) {
-            cin >> a >> b;
-            int flag = 0;
-            deque<int> &t = v[now[a]];
-            deque<int> u;
-            while (t.back() != a)
-                u.push_front(t.back()), now[t.back()] += b, t.pop_back();
-            u.push_front(t.back()), now[t.back()] += b, t.pop_back();
-            while (!v[now[a]].empty()) {
-                u.push_front(v[now[a]].back());
-                v[now[a]].pop_back();
-            }
-            v[now[a]] = u;
-        }
-        if (v[9].size() == 3) cout << "Y" << endl;
-        else cout << "N" << endl;
+#include<bits/stdc++.h>
+#include<functional>
+#define int long long
+#define endl "\n"
+const int N = 2e5 + 10;
+const int mod = 1e9 + 7;
+const int INF = INT64_MAX;
+#define fs(x) std::fixed << std::setprecision(x)
+using ld = long double;
+struct Point3 {
+    ld x, y, z;
+    Point3(ld x_ = 0, ld y_ = 0, ld z_ = 0) : x(x_), y(y_), z(z_) {}
+    Point3 &operator+=(Point3 p)& {
+        return x += p.x, y += p.y, z += p.z, *this;
     }
+    Point3 &operator-=(Point3 p)& {
+        return x -= p.x, y -= p.y, z -= p.z, *this;
+    }
+    Point3 &operator*=(Point3 p)& {
+        return x *= p.x, y *= p.y, z *= p.z, *this;
+    }
+    Point3 &operator*=(ld t)& {
+        return x *= t, y *= t, z *= t, *this;
+    }
+    Point3 &operator/=(ld t)& {
+        return x /= t, y /= t, z /= t, *this;
+    }
+    friend Point3 operator+(Point3 a, Point3 b) {
+        return a += b;
+    }
+    friend Point3 operator-(Point3 a, Point3 b) {
+        return a -= b;
+    }
+    friend Point3 operator*(Point3 a, Point3 b) {
+        return a *= b;
+    }
+    friend Point3 operator*(Point3 a, ld b) {
+        return a *= b;
+    }
+    friend Point3 operator*(ld a, Point3 b) {
+        return b *= a;
+    }
+    friend Point3 operator/(Point3 a, ld b) {
+        return a /= b;
+    }
+    friend auto &operator>>(std::istream &is, Point3 &p) {
+        return is >> p.x >> p.y >> p.z;
+    }
+    friend auto &operator<<(std::ostream &os, Point3 p) {
+        return os << "(" << p.x << ", " << p.y << ", " << p.z << ")";
+    }
+};
+struct Line3 {
+    Point3 a, b;
+};
+struct Plane {
+    Point3 u, v, w;
+};
+inline void solve() {
+    int n;
+    std::cin >> n;
+    std::string s;
+    std::cin >> s;
+    std::map<std::pair<int, int>, int> mp;
+    std::set<std::pair<int, int>> ans;
+    mp[ {0, 0}] = 1;
+    std::function<void(int, int, int)> dfs = [&](int x, int y, int cnt) {
+        if (cnt == n) {
+            ans.insert({x, y});
+            return;
+        }
+        int dx = x, dy = y;
+        if (s[cnt] == 'R')dx++;
+        else if (s[cnt] == 'L') dx--;
+        else if (s[cnt] == 'U') dy++;
+        else dy--;
+        if (mp[ {dx, dy}] == 0) {
+            mp[ {dx, dy}] = 1;
+            dfs(dx, dy, cnt + 1);
+            mp[ {dx, dy}] = -1;
+            dfs(x, y, cnt + 1);
+            mp[ {dx, dy}] = 0;
+        } else if (mp[ {dx, dy}] == 1) {
+            dfs(dx, dy, cnt + 1);
+        } else dfs(x, y, cnt + 1);
+    };
+    dfs(0, 0, 0);
+    std::cout << ans.size() << endl;
+    for (auto &[x, y] : ans) std::cout << x << " " << y << endl;
+}
+signed main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    int t = 1;
+    //std:: cin >> t;
+    while (t--) solve();
     return 0;
 }
